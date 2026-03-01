777eae4f733d5a0da2013fbbeaf905f63e02f44c
Test User <test@example.com>
2026-03-01 20:54:57 +0530

    fix wsgi application variable

--- wsgi.py
+++ wsgi.py
@@ -10,4 +10,6 @@ except ImportError:
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from backend.app_supabase import app as application
+from backend.app_supabase import app
+
+application = app
